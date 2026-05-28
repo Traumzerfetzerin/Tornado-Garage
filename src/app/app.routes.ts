f48aac35062f0da7c004cfddb62790e84494c6af
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-05-28 20:54:24 +0200

    feat: fix structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-// import { ImprintComponent } from './shared/imprint/imprint.component';
+import { ImprintComponent } from './share/imprint/imprint.component';
 import { PolicyComponent } from './share/policy/policy.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    // { path: 'imprint', component: ImprintComponent },
+    { path: 'imprint', component: ImprintComponent },
     { path: 'policy', component: PolicyComponent },
     { path: '**', redirectTo: '' }
 ];
